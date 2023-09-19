1bd47c72f93c506775cc4163c5b022cc1c657f25
lonewanderer27 <adrianejames03@gmail.com>
2023-09-19 11:50:19 +0800

    init ui

---
+++ app/src/main/java/com/jay_puzon/puzon/PuzonActivity.java
@@ -0,0 +1,14 @@
+package com.jay_puzon.puzon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PuzonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzon);
+    }
+}
